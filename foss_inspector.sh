9ddd3061229c22599baceb1c5f24c6c0d3a1c2bc
himanshuvatsha <vatsha2006@gmail.com>
2026-03-31 23:13:00 +0530

    Update foss_inspector.sh

--- foss_inspector.sh
+++ foss_inspector.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # ==============================================================
 # Script 2: FOSS Package Inspector
-# Author: siddharth jain| Course: Open Source Software
+# Author: Himanshu vatsha| Course: Open Source Software
 # Purpose: Checks whether a specified open-source package is
 #          installed on the system, displays its version and
 #          license info, and prints a short philosophy note

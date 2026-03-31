0b63db69de87f29a6ef0a60464e76cc61a0c494d
himanshuvatsha <vatsha2006@gmail.com>
2026-03-31 23:13:39 +0530

    Update log_analyzer.sh

--- log_analyzer.sh
+++ log_analyzer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # ==============================================================
 # Script 4: Log File Analyzer
-# Author: siddharth jain| Course: Open Source Software
+# Author: Himanshu vatsha| Course: Open Source Software
 # Purpose: Reads a log file line by line, counts occurrences
 #          of a keyword (like ERROR or WARNING), prints a
 #          summary, and shows the last 5 matching lines.
